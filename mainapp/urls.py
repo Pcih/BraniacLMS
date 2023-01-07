ecddf2650c02e111f42f5f42f95ab386c7a06136
pcih <pcih1984@gmail.com>
2023-01-08 00:44:38 +0300

    lesson_2 Code style fix

--- mainapp/urls.py
+++ mainapp/urls.py
@@ -6,10 +6,10 @@ from mainapp.apps import MainappConfig
 app_name = MainappConfig.name
 
 urlpatterns = [
-    path('', views.MainPageView.as_view()),
-    path('news/', views.NewsPageView.as_view()),
-    path('coutses/', views.CoursesPageView.as_view()),
-    path('contacts/', views.ContactsPageView.as_view()),
-    path('doc_site/', views.DocSitePageView.as_view()),
-    path('login.html/', views.LoginPageView.as_view()),
+    path("", views.MainPageView.as_view()),
+    path("news/", views.NewsPageView.as_view()),
+    path("coutses/", views.CoursesPageView.as_view()),
+    path("contacts/", views.ContactsPageView.as_view()),
+    path("doc_site/", views.DocSitePageView.as_view()),
+    path("login.html/", views.LoginPageView.as_view()),
 ]
