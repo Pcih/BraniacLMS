584b3ef8e948aabed0c0030b2da0d1f355b47841
Pcih <85923509+Pcih@users.noreply.github.com>
2023-01-08 14:27:52 +0300

    Lesson 2 (#1)
    * lesson_2 Hello world with CBV and FBV
    
    * lesson_2 Static site with Django
    
    * lesson_2 Code style fix
    
    * lesson_3 Template's context
    
    * lesson_2 delete key

--- mainapp/views.py
+++ mainapp/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from django.views.generic import TemplateView
 
-# Create your views here.
+
+class MainPageView(TemplateView):
+    template_name = "mainapp/index.html"
+
+
+class NewsPageView(TemplateView):
+    template_name = "mainapp/news.html"
+
+    #  Получить все предыдущие данные
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # Создайте свои собственные данные
+        context["news_title"] = "Громкий новостной заголовок"
+        context["news_preview"] = "Предварительное описание, которое заинтересует каждого"
+        return context
+
+
+class CoursesPageView(TemplateView):
+    template_name = "mainapp/courses_list.html"
+
+
+class ContactsPageView(TemplateView):
+    template_name = "mainapp/contacts.html"
+
+
+class DocSitePageView(TemplateView):
+    template_name = "mainapp/doc_site.html"
+
+
+class LoginPageView(TemplateView):
+    template_name = "mainapp/login.html"
